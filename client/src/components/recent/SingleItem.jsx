d61ebb6e1c3f70f667abf9f2e3adddc40c13dd89
martinski74 <martin.dobr@abv.bg>
2025-04-07 16:00:09 +0300

    update

--- client/src/components/recent/SingleItem.jsx
+++ client/src/components/recent/SingleItem.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
-import truncate from '../../util/truncate';
 import styles from './SingleItem.module.css';
 const SingleItem = ({ item }) => {
   return (
     <div className={styles.item}>
       <img className={styles.image} src={item.image} />
-      <p>{truncate(item.title, 20)}</p>
+      <p>{item.title}</p>
 
       <Link className={styles['details-btn']} to={`/catalog/${item._id}`}>
         Details
